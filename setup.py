6fe2f29fe9cb7ee6584d484d1162dc2938e66c04
luowf <louwf@dp.tech>
2023-04-03 17:15:06 +0800

    git status

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit-antd",
-    version="0.7.2",
+    version="0.7.1",
     author="mapix",
     author_email="mapix.me@gmail.com",
     description="",
