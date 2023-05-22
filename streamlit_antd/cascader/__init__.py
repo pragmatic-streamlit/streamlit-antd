0912881ce459b3dacb4b1fdaf796c30232ac7ee8
DingShinn <dingyubo@dp.tech>
2023-05-22 17:39:36 +0800

    fix: 修复 cascader 无法传递 on_change 参数的问题

--- streamlit_antd/cascader/__init__.py
+++ streamlit_antd/cascader/__init__.py
@@ -1,5 +1,7 @@
 import os
+import streamlit as st
 import streamlit.components.v1 as components
+from typing import Any
 
 _DEVELOP_MODE = os.getenv('DEVELOP_MODE')
 # _DEVELOP_MODE = True
@@ -16,8 +18,24 @@ else:
     _component_func = components.declare_component("streamlit_antd_cascader", path=build_dir)
 
 
+def set_session_value(key: str, value: Any):
+    st.session_state[key] = value
+    
+def get_session_value(key):
+    if key not in st.session_state:
+        return None
+    return st.session_state[key]
+
 def st_antd_cascader(options, multiple=False, key=None, **kwargs):
+    on_change = kwargs.get("on_change", None)
+    value_key = f"{key}_value"
+    if on_change:
+        del kwargs["on_change"]
+    prev_value = get_session_value(value_key) if on_change else None
     component_value = _component_func(options=options, multiple=multiple, key=key, default=None, **kwargs)
+    if component_value != prev_value:
+        set_session_value(value_key, component_value)
+        on_change and on_change(component_value)
     return component_value
 
 
